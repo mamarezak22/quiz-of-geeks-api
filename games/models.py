e1cda9f1810b02d38b95436effe0c3114281e0f7
mamarezak22 <mohammadreza13842018@gmail.com>
2025-04-27 15:59:58 +0330

    settin up games app models

--- games/models.py
+++ games/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from questions.models import Category, Question
+from users.models import User
+
+class Game(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.SET_NULL)
+    user2 = models.ForeignKey(User, on_delete=models.SET_NULL,null=True,blank = True)
+    current_round = models.PositiveSmallIntegerField(default=0)
+    user1_point = models.PositiveSmallIntegerField(default=0)
+    user2_point = models.PositiveSmallIntegerField(default=0)
+    shown_categories = models.ManyToManyField(Category)
+    current_user_turn = models.ForeignKey(User, on_delete=models.SET_NULL,null=True,blank = True)
+    last_turn_time = models.DateTimeField(default=None, null=True,blank = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    ended_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return f'{self.user1} vs. {self.user2}'
+
+class Round(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE,related_name='rounds')
+    round_number = models.PositiveSmallIntegerField(default = None,null=True,blank = True)
+    selected_category = models.ForeignKey(Category, on_delete=models.SET_NULL,null=True,blank = True)
+    questions = models.ManyToManyField(Question,through = "GameQuestion")
+    count_of_answered_question_by_user1 = models.PositiveSmallIntegerField(default = 0)
+    count_of_answered_question_by_user2 = models.PositiveSmallIntegerField(default = 0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    ended_at  = models.DateTimeField(null=True)
+
+class GameQuestion(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE,related_name='questions')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,related_name='questions')
+    question_number = models.PositiveSmallIntegerField()
+    is_user1_answered = models.BooleanField(default=False)
+    is_user2_answered = models.BooleanField(default=False)
+    user1_answer_time = models.DurationField(null = True,blank = True)
+    user2_answer_time = models.DurationField(null = True,blank = True)
