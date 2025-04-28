050cd145943e9366ea5f58522c454045acda6641
mamarezak22 <mohammadreza13842018@gmail.com>
2025-04-28 15:17:29 +0330

    update models for games

--- games/models.py
+++ games/models.py
@@ -6,8 +6,8 @@ from users.models import User
 
 class Game(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user1 = models.ForeignKey(User, on_delete=models.SET_NULL)
-    user2 = models.ForeignKey(User, on_delete=models.SET_NULL,null=True,blank = True)
+    user1 = models.ForeignKey(User, on_delete=models.SET_NULL,null = True,blank = True,related_name="game_as_user_1")
+    user2 = models.ForeignKey(User, on_delete=models.SET_NULL,null=True,blank = True,related_name="game_as_user_2")
     current_round = models.PositiveSmallIntegerField(default=0)
     user1_point = models.PositiveSmallIntegerField(default=0)
     user2_point = models.PositiveSmallIntegerField(default=0)
@@ -25,17 +25,17 @@ class Round(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE,related_name='rounds')
     round_number = models.PositiveSmallIntegerField(default = None,null=True,blank = True)
     selected_category = models.ForeignKey(Category, on_delete=models.SET_NULL,null=True,blank = True)
-    questions = models.ManyToManyField(Question,through = "GameQuestion")
+    questions = models.ManyToManyField(Question,through = "QuestionRound")
     count_of_answered_question_by_user1 = models.PositiveSmallIntegerField(default = 0)
     count_of_answered_question_by_user2 = models.PositiveSmallIntegerField(default = 0)
     created_at = models.DateTimeField(auto_now_add=True)
     ended_at  = models.DateTimeField(null=True)
 
-class GameQuestion(models.Model):
-    game = models.ForeignKey(Game, on_delete=models.CASCADE,related_name='questions')
-    question = models.ForeignKey(Question, on_delete=models.CASCADE,related_name='questions')
+class QuestionRound(models.Model):
+    round = models.ForeignKey(Round, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     question_number = models.PositiveSmallIntegerField()
     is_user1_answered = models.BooleanField(default=False)
     is_user2_answered = models.BooleanField(default=False)
-    user1_answer_time = models.DurationField(null = True,blank = True)
-    user2_answer_time = models.DurationField(null = True,blank = True)
+    user1_seen_time = models.DateTimeField(null = True,blank = True)
+    user2_seen_time = models.DateTimeField(null = True,blank = True)
