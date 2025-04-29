6cdcfd2751d3dc389ea1611b8acebc5f150924e3
mamarezak22 <mohammadreza13842018@gmail.com>
2025-04-29 18:07:47 +0330

    add custom save method in game model for handling current_user_turn field better.

--- games/models.py
+++ games/models.py
@@ -17,6 +17,11 @@ class Game(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     ended_at = models.DateTimeField(null=True)
 
+   #the current_user_turn in first of game is user1
+    def save(self, *args, **kwargs):
+        if not self.current_user_turn:
+            self.current_user_turn = self.user1
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.user1} vs. {self.user2}'
@@ -25,13 +30,16 @@ class Round(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE,related_name='rounds')
     round_number = models.PositiveSmallIntegerField(default = None,null=True,blank = True)
     selected_category = models.ForeignKey(Category, on_delete=models.SET_NULL,null=True,blank = True)
-    questions = models.ManyToManyField(Question,through = "QuestionRound")
+    questions = models.ManyToManyField(Question,through = "RoundQuestion")
     count_of_answered_question_by_user1 = models.PositiveSmallIntegerField(default = 0)
     count_of_answered_question_by_user2 = models.PositiveSmallIntegerField(default = 0)
     created_at = models.DateTimeField(auto_now_add=True)
     ended_at  = models.DateTimeField(null=True)
 
-class QuestionRound(models.Model):
+    def __str__(self):
+        return f"round {self.round_number} of game {self.game}"
+
+class RoundQuestion(models.Model):
     round = models.ForeignKey(Round, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     question_number = models.PositiveSmallIntegerField()
@@ -39,3 +47,6 @@ class QuestionRound(models.Model):
     is_user2_answered = models.BooleanField(default=False)
     user1_seen_time = models.DateTimeField(null = True,blank = True)
     user2_seen_time = models.DateTimeField(null = True,blank = True)
+
+    def __str__(self):
+        return f"question {self.question_number} of round {self.round}"
